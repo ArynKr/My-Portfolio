57cc8d6c84afd82f3dd3094be91a9dde7ce5d345
Aryan Kumar <aryankumar1504@gmail.com>
2021-02-28 15:38:18 +0530

    Home section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import {ReactComponent as Avatar} from "./images/avatar.svg";
+import {ReactComponent as Setup} from "./images/setup.svg";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <div className="home">
+        <nav>
+          <Navbar />
+        </nav>
+        <main className="profile">
+          <div className="left">
+            <div className="fname">ARYAN</div>
+            <div className="lname">KUMAR</div>
+            <div className="typing">FULL STACK WEB DEVELOPER</div>
+            <button>RESUME</button>
+          </div>
+          <div className="right">
+            <Avatar />
+          </div>
+        </main>
+        <div className="svg">
+          <Setup />
+        </div>
+      </div>
     </div>
   );
 }
